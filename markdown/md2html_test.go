bbc665bd4c1b17cf0d72b36025141310786d38cd
Hector Correa <hector@hectorcorrea.com>
2017-07-23 17:51:19 -0400

    Commented unused test

--- markdown/md2html_test.go
+++ markdown/md2html_test.go
@@ -1,25 +1,25 @@
 package markdown
 
-import (
-	"io/ioutil"
-	"testing"
-
-	"hk/markdown"
-)
-
-func TestText(t *testing.T) {
-	md := loadFromDisk()
-	var parser markdown.Parser
-	html := parser.ToHtml(md)
-	saveToDisk(html)
-	t.Errorf("%s", html)
-}
-
-func loadFromDisk() string {
-	bytes, _ := ioutil.ReadFile("test.md")
-	return string(bytes)
-}
-
-func saveToDisk(html string) {
-	ioutil.WriteFile("test.html", []byte(html), 0644)
-}
+// import (
+// 	"io/ioutil"
+// 	"testing"
+//
+// 	"hk/markdown"
+// )
+//
+// func TestText(t *testing.T) {
+// 	md := loadFromDisk()
+// 	var parser markdown.Parser
+// 	html := parser.ToHtml(md)
+// 	saveToDisk(html)
+// 	t.Errorf("%s", html)
+// }
+//
+// func loadFromDisk() string {
+// 	bytes, _ := ioutil.ReadFile("test.md")
+// 	return string(bytes)
+// }
+//
+// func saveToDisk(html string) {
+// 	ioutil.WriteFile("test.html", []byte(html), 0644)
+// }
