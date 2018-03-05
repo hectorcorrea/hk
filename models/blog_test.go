d1072bbf476d68c956644a7045a4c7a81c830e06
Hector Correa <hector@hectorcorrea.com>
2018-03-04 20:50:09 -0500

    Better handle some legacy links for pictures and account for images not stored in AWS

--- models/blog_test.go
+++ models/blog_test.go
@@ -1,6 +1,10 @@
 package models
 
-import "testing"
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
 
 func TestSlug(t *testing.T) {
 	testA := []string{"", ""}
@@ -23,14 +27,39 @@ func TestSlug(t *testing.T) {
 	}
 }
 
-// func TestLegacySlug(t *testing.T) {
-// 	legacySlug := "Something-ABC.aspx"
-// 	slug := strings.ToLower(legacySlug)
+// func TestLegacyViewPicture(t *testing.T) {
+// 	part1 := "before1 <a href=aaa>bbb<img src=ccc /></a> after1"
+// 	part2 := "before2 <a href=xxx>yyy<img src=zzz /></a> after2"
+// 	testHtml := part1 + part2
+// 	// testHtml = "before <a href=\"javascript:viewpicture('Paris+2008','pictures/2008/halloween_043.jpg')\"><img border=\"1\" alt=\"\" src=\"http://www.hectorykarla.com/photos/2008/halloween_043_thumb.jpg\" /></a> after"
+// 	reViewPicture := regexp.MustCompile("<a href=(.*?)>(.*?)</a>")
+// 	matches := reViewPicture.FindAllString(testHtml, -1)
 //
-// 	if strings.HasSuffix(slug, ".aspx") {
-// 		slug = slug[0 : len(slug)-5]
-// 		t.Errorf("(1) %s", slug)
-// 	} else {
-// 		t.Errorf("(2) %s", slug)
+// 	// match
+// 	// <a href=viewpic(...)><img src=yyy /></a>
+// 	// 0123456789-123456789-123456789-123456789
+//
+// 	for i, match := range matches {
+// 		img := ""
+// 		jsViewPicture := strings.Index(match, "javascript:viewpicture")
+// 		if jsViewPicture != -1 {
+// 			imgBegin := strings.Index(match, "<img")
+// 			if imgBegin > jsViewPicture {
+// 				imgEnd := strings.Index(match[imgBegin:], "/>")
+// 				if imgEnd > imgBegin {
+// 					// a := imgBegin
+// 					// b := imgBegin + imgEnd + 2
+// 					// t.Errorf("%d %d %d", i, a, b)
+// 					img = match[imgBegin:(imgBegin + imgEnd + 2)]
+// 				}
+// 			}
+// 		}
+// 		t.Errorf("%d %s", i, img)
+// 		if img != "" {
+// 			testHtml = strings.Replace(testHtml, match, img, 1)
+// 		}
 // 	}
+//
+// 	// str := x[0] //+ " ** " + x[1]
+// 	t.Errorf("%s", testHtml)
 // }
